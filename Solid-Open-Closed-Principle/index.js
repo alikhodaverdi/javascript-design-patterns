e6dc26cbf4d6331420770758154fc60594b7b5f5
al <alikhodaveerdi@yahoo.com>
2023-02-12 12:23:56 +0330

    open-close-principle-start

---
+++ Solid-Open-Closed-Principle/index.js
@@ -0,0 +1,12 @@
+let Color = Object.freeze({
+  red: "red",
+  green: "green",
+});
+
+class Product {
+  constructor(name, color, size) {
+    this.name = name;
+    this.color = color;
+    this.size = size;
+  }
+}
